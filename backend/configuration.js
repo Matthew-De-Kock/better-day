584c45f52ed8f0d5df0e5a5060bd0fbdaea2632d
Matthew De Kock <82797290+Matthew-De-Kock@users.noreply.github.com>
2022-04-26 12:51:59 +0200

    test pull

--- backend/configuration.js
+++ backend/configuration.js
@@ -1,6 +1,16 @@
 var connectionStringStandard = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 var connectionStringMongoose = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/Betterday?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
+
+// var connectionStringStandard = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// var connectionStringMongoose = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/Betterday?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+// var connectionStringStandard = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// var connectionStringMongoose = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/Betterday?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+// var connectionStringStandard = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// var connectionStringMongoose = "mongodb://betterday:M%40CAutomation1@192.168.2.251:27017/Betterday?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
 module.exports= {
     connectionStringMongoose,
     connectionStringStandard
